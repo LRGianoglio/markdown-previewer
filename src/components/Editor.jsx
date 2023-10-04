053a18baf8e9d457f2d91e1fb1327c85e35392da
unknown <luchogianoglio@gmail.com>
2023-10-04 10:05:17 -0300

    feat/ added minimized styles

--- src/components/Editor.jsx
+++ src/components/Editor.jsx
@@ -8,7 +8,6 @@ function Editor() {
 
   const innerText = useSelector((state) => state.html);
   const editorDisplay = useSelector((state) => state.editorDisplay);
-  const previewerDisplay = useSelector((state) => state.previewerDisplay);
 
   const handleTyping = (event) => {
     dispatch(setInnerHTML(event.target.value))
@@ -20,20 +19,39 @@ function Editor() {
       dispatch(maximizeEditor())
   }
 
-  const estilo = 
-    editorDisplay ?
-    "h-10 bg-teal-900 flex border-b-2 border-teal-500 justify-between items-center px-4 font-semibold text-teal-500" :
-    "h-10 bg-teal-800 flex justify-between items-center px-4 font-semibold text-red-300"
+  //! -------------------------ESTILOS----------------------------------
+
+  let styles = {
+    mainDiv: "h-5/6 w-5/12 border-teal-500 border-2 shadow-2xl transition-all ease-in-out duration-300",
+    toolbar: "h-10 bg-teal-900 flex border-b-2 border-teal-500 justify-between items-center px-4 font-semibold text-teal-500",
+    window: "h-window w-full bg-slate-800"
+  }
+
+  if(editorDisplay){
+    styles = {
+      mainDiv: "h-5/6 w-5/12 border-teal-500 border-2 shadow-2xl transition-all ease-in-out duration-300",
+      toolbar: "h-10 bg-teal-900 flex border-b-2 border-teal-500 justify-between items-center px-4 font-semibold text-teal-500",
+      window: "h-window w-full bg-slate-800"
+    }
+  } else {
+    styles = {
+      mainDiv: "h-24 w-24 bg-teal-900 shadow-2xl transition-all ease-in-out duration-300 fixed left-0 top-1/2 transform -translate-y-1/2 rounded-tr-xl rounded-br-xl",
+      toolbar: "h-24 w-24 bg-teal-900 flex flex-col justify-evenly items-center px-4 font-semibold text-teal-500 rounded-tr-xl rounded-br-xl",
+      window: "hidden"
+    }
+  }
+
+  //! ------------------------------------------------------------------
 
   return (
-    <div className="h-5/6 w-5/12 border-teal-500 border-2 shadow-2xl">
-      <div className={estilo}>
+    <div className={styles.mainDiv}>
+      <div className={styles.toolbar}>
         <p className='text-xl'>Editor</p>
         <button onClick={handleMinMax}>
           {editorDisplay ? <BsArrowBarLeft className='text-2xl'/> : <BsArrowBarRight className='text-2xl'/>}
         </button>
       </div>
-      <div className='h-window w-full bg-slate-800'>
+      <div className={styles.window}>
         <textarea cols="30" rows="10" value={innerText} onChange={handleTyping} className='w-full h-full bg-slate-800 text-slate-200 p-4'></textarea>
       </div>
     </div>
--- src/components/Previewer.jsx
+++ src/components/Previewer.jsx
@@ -21,20 +21,39 @@ function Previewer() {
       dispatch(maximizePreviewer())
   }
 
-  const estilo = 
-    previewerDisplay ?
-    "h-10 bg-teal-900 flex border-b-2 border-teal-500 justify-between items-center px-4 font-semibold text-teal-500" :
-    "h-10 bg-teal-800 flex justify-between items-center px-4 font-semibold text-red-300"
+    //! -------------------------ESTILOS----------------------------------
+
+    let styles = {
+      mainDiv: "h-5/6 w-5/12 border-teal-500 border-2 shadow-2xl transition-all ease-in-out duration-300",
+      toolbar: "h-10 bg-teal-900 flex border-b-2 border-teal-500 justify-between items-center px-4 font-semibold text-teal-500",
+      window: "h-window w-full bg-slate-800 overflow-y-scroll"
+    }
+  
+    if(previewerDisplay){
+      styles = {
+        mainDiv: "h-5/6 w-5/12 border-teal-500 border-2 shadow-2xl transition-all ease-in-out duration-300",
+        toolbar: "h-10 bg-teal-900 flex border-b-2 border-teal-500 justify-between items-center px-4 font-semibold text-teal-500",
+        window: "h-window w-full bg-slate-800 overflow-y-scroll"
+      }
+    } else {
+      styles = {
+        mainDiv: "h-24 w-24 bg-teal-900 shadow-2xl transition-all ease-in-out duration-300 fixed right-0 top-1/2 transform -translate-y-1/2 rounded-tl-xl rounded-bl-xl",
+        toolbar: "h-24 w-24 bg-teal-900 flex flex-col justify-evenly items-center px-4 font-semibold text-teal-500 rounded-tl-xl rounded-bl-xl",
+        window: "hidden"
+      }
+    }
+  
+    //! ------------------------------------------------------------------
 
   return (
-    <div className="h-5/6 w-5/12 border-teal-500 border-2 shadow-2xl">
-      <div className={estilo}>
+    <div className={styles.mainDiv}>
+      <div className={styles.toolbar}>
         <button onClick={handleMinMax}>
           {previewerDisplay ? <BsArrowBarRight className='text-2xl'/> : <BsArrowBarLeft className='text-2xl'/>}
         </button>
         <p className='text-xl'>Previewer</p>
       </div>
-      <div className='h-window w-full bg-slate-800 p-4 overflow-y-scroll'>
+      <div className={styles.window}>
         <p dangerouslySetInnerHTML={{ __html: marked(innerText)}} className='text-slate-200'></p>
       </div>
     </div>
